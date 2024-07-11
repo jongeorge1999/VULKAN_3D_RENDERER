17bb9f9e2ca881a87f1e1d29a0dfdbe344dc0d5f
jongeorge1999 <jongeorge1999@gmail.com>
2024-07-11 13:31:28 -0400

    Added support for linux

--- build/compile.sh
+++ build/compile.sh
@@ -1,2 +1,2 @@
-C:/VulkanSDK/1.3.280.0/Bin/glslc.exe ../shaders/simple_shader.vert -o ../shaders/simple_shader.vert.spv
-C:/VulkanSDK/1.3.280.0/Bin/glslc.exe ../shaders/simple_shader.vert -o ../shaders/simple_shader.fragment.spv
+/usr/bin/glslc ../shaders/simple_shader.vert -o ../shaders/simple_shader.vert.spv
+/usr/bin/glslc ../shaders/simple_shader.frag -o ../shaders/simple_shader.fragment.spv
