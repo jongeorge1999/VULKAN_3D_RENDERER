51a02338fcfe8ccc1252c54c3a3626ee2a835db2
Jonathan George <jongeorge1999@gmail.com>
2024-07-13 15:16:08 -0400

    3D model loading, and index buffer implemented

--- src/Model.cpp
+++ src/Model.cpp
@@ -1,7 +1,26 @@
 #include "Model.hpp"
+#include "Utils.hpp"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tinyobjloader/tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
 #include <cassert>
 #include <cstring>
+#include <iostream>
+#include <unordered_map>
+
+namespace std {
+    template <>
+    struct hash<Model::Vertex> {
+        size_t operator()(Model::Vertex const&vertex) const {
+            size_t seed = 0;
+            hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+            return seed;
+        }
+    };
+}
 
 Model::Model(Device& device, const Model::Builder &builder) : device{device} {
     createVertexBuffers(builder.vertices);
@@ -16,6 +35,13 @@ Model::~Model(){
         vkFreeMemory(device.device(), indexBufferMemory, nullptr);
     }
 
+}
+
+std::unique_ptr<Model> Model::createModelFromFile(Device& device, const std::string& filepath) {
+    Builder builder{};
+    builder.loadModel(filepath);
+    std::cout << "Vertex count: " << builder.vertices.size() << "\n";
+    return std::make_unique<Model>(device, builder);
 }
 
 void Model::createVertexBuffers(const std::vector<Vertex> &vertices) {
@@ -126,4 +152,67 @@ std::vector<VkVertexInputAttributeDescription> Model::Vertex::getAttributeDescri
     attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
     attributeDescriptions[1].offset = offsetof(Vertex, color);
     return attributeDescriptions;
+}
+
+void Model::Builder::loadModel(const std::string& filepath) {
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn;
+    std::string err;
+
+    if(!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+        throw std::runtime_error(warn + err);
+    }
+
+    vertices.clear();
+    indices.clear();
+
+    std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
+    for(const auto &shape : shapes) {
+        for (const auto &index : shape.mesh.indices) {
+            Vertex vertex{};
+
+            if(index.vertex_index >= 0) {
+                vertex.position = {
+                    attrib.vertices[3 * index.vertex_index + 0],
+                    attrib.vertices[3 * index.vertex_index + 1],
+                    attrib.vertices[3 * index.vertex_index + 2],
+                };
+
+                auto colorIndex = 3 * index.vertex_index + 2;
+                if(colorIndex < attrib.colors.size()) {
+                    vertex.color = {
+                        attrib.colors[colorIndex - 2],
+                        attrib.colors[colorIndex - 1],
+                        attrib.colors[colorIndex - 0],
+                    };
+                } else {
+                    vertex.color = {1.f,1.f,1.f};
+                }
+            }
+
+            if(index.normal_index >= 0) {
+                vertex.normal = {
+                    attrib.normals[3 * index.normal_index + 0],
+                    attrib.normals[3 * index.normal_index + 1],
+                    attrib.normals[3 * index.normal_index + 2],
+                };
+            }
+
+            if(index.texcoord_index >= 0) {
+                vertex.uv = {
+                    attrib.texcoords[2 * index.texcoord_index + 0],
+                    attrib.texcoords[2 * index.texcoord_index + 1],
+                };
+            }
+
+            if(uniqueVertices.count(vertex) == 0) {
+                uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                vertices.push_back(vertex);
+            }
+            indices.push_back(uniqueVertices[vertex]);
+        }
+    }
 }
